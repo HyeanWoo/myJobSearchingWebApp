62577bbdbb23a9a56404a63185b8b868613a728e
unknown <wkdus7h@naver.com>
2020-05-20 16:57:57 +0900

    react skeleton code done

--- redux_practice/src/components/projects/ProjectList.js
+++ redux_practice/src/components/projects/ProjectList.js
@@ -1,35 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text">18th May, 4pm</p>
-        </div>
-      </div>
-      
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text">18th May, 4pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text">18th May, 4pm</p>
-        </div>
-      </div>
-
+      <ProjectSummary/>
+      <ProjectSummary/>
+      <ProjectSummary/>
+      <ProjectSummary/>
     </div>
-  );
+  )
 }
 
-export default ProjectList;
+export default ProjectList
