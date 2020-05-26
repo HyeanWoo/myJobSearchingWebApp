c33cf4fc41fa3c1aab42283a73648bbf65c3344d
unknown <wkdus7h@naver.com>
2020-05-26 23:51:28 +0900

    using firebase reducer

--- redux_practice/src/components/dashboard/Dashboard.js
+++ redux_practice/src/components/dashboard/Dashboard.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class Dashboard extends Component {
   render() {
+    const { projects } = this.props;
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList projects={this.props.projects}/>
+            <ProjectList projects={projects}/>
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -20,10 +24,16 @@ class Dashboard extends Component {
   }
 }
 
-const mapStateToprops = (root) => {
+const mapStateToprops = (state) => {
+  console.log(state.firestore.ordered.projects)
   return {
-    projects: root.project.projects
+    projects: state.firestore.ordered.projects || state.project.projects
   }
 }
 
-export default connect(mapStateToprops)(Dashboard);
+export default compose(
+  connect(mapStateToprops),
+  firestoreConnect([
+    { collection: 'projects'}
+  ])
+)(Dashboard);
